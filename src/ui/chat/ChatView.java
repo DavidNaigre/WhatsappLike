9e79f894d51c4f16dda520a937a985934a523e25
David <dnaigre.univ@gmail.com>
2020-05-28 13:17:51 -0400

    Chat xml
    Signed-off-by: David <dnaigre.univ@gmail.com>

--- src/ui/chat/ChatView.java
+++ src/ui/chat/ChatView.java
@@ -1,4 +1,29 @@
 package ui.chat;
 
-public class ChatView {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatView implements Initializable {
+    @FXML private TextField searchInput, messageInput;
+    @FXML private Label userName, contactName;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleEnterSearchKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void handleEnterSendKeyReleased(KeyEvent keyEvent) {
+    }
+
+    public void handleSendMessageClick(MouseEvent mouseEvent) {
+    }
 }
